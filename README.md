6d00eaf13424f9d7588f6ff045574eb2818825f9
rroland20 <alena_tumanova96@mail.ru>
2023-05-29 17:18:51 +0400

    update readme and app-info.js

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Employees
 Employee accounting app on React
+
+
+# Установка и запуск
+Клонируйте проект на ваш компьютер, далее перейдите в папку и введите команды по очереди:
+npm i
+npm start
+
--- src/components/app-info/app-info.js
+++ src/components/app-info/app-info.js
@@ -3,7 +3,7 @@ import './app-info.css';
 const AppInfo = ({employees, increased}) => {
     return (
         <div className="app-info">
-            <h1>Учет сотрудников в компании Алена и Ко</h1>
+            <h1>Учет сотрудников в компании N</h1>
             <h2>Общее число сотрудников: {employees}</h2>
             <h2>Премию получат: {increased}</h2>
         </div>
